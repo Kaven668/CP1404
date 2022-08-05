be43a1dc2f9476f6b6c044413401f42f81f1f263
billkaven <kaven668@gmail.com>
2022-08-05 10:02:25 +0800

    change

---
+++ prac_4/lists_warmup.py
@@ -0,0 +1,16 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 1
+numbers[0] = "ten"
+print(numbers)
+
+# 2
+numbers[-1] = 1
+print(numbers)
+
+# 3
+
+
+
+# 4
+print(9 in numbers)
---
+++ prac_4/total_income.py
@@ -0,0 +1,38 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+
+incomes = []
+
+def main():
+    """Display income report for incomes over a given mounth_number of months."""
+    months = int(input("How many months? "))
+    for mounth_number in range(1, months + 1):
+        income = float(input("Enter income for mounth_number {}:".format(str(mounth_number)) ))
+        incomes.append(income)
+    return months
+
+def get_total_incomes():
+    total = 0
+    
+    print("\nIncome Report\n-------------")
+    for mounth_number in range(1, months + 1):
+        income = incomes[mounth_number - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(mounth_number, income, total))
+
+
+
+
+get_total_incomes()
+
+
+
+
+
+
+
+main()
